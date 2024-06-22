186c555a709e88982e9ae8e706208d96d0441ed6
TajulMunandar <tajulmunandar701@gmail.com>
2024-06-22 18:19:57 +0700

    list item icon

--- resources/js/Components/dashboard/components/sidebars/ListItems.tsx
+++ resources/js/Components/dashboard/components/sidebars/ListItems.tsx
@@ -29,7 +29,9 @@ const ListItems: React.FC = () => {
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/registrasi")}`}>
+                <li
+                    className={`menu-item ${isActive("/dashboard/registrasi")}`}
+                >
                     <Link href="/dashboard/registrasi" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
@@ -44,48 +46,76 @@ const ListItems: React.FC = () => {
                     <Link href="/dashboard/kunjungan" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "address-card"]}
+                            icon={["fas", "chart-line"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Kunjungan</div>
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/transaksi-obat")}`}>
-                    <Link href="/dashboard/transaksi-obat" className="menu-link">
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/transaksi-obat"
+                    )}`}
+                >
+                    <Link
+                        href="/dashboard/transaksi-obat"
+                        className="menu-link"
+                    >
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "address-card"]}
+                            icon={["fas", "tablets"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Transaksi Obat</div>
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/konsultasi-online")}`}>
-                    <Link href="/dashboard/konsultasi-online" className="menu-link">
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/konsultasi-online"
+                    )}`}
+                >
+                    <Link
+                        href="/dashboard/konsultasi-online"
+                        className="menu-link"
+                    >
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "address-card"]}
+                            icon={["fas", "comment"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Konsultasi Online</div>
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/transaksi-obat-online")}`}>
-                    <Link href="/dashboard/transaksi-obat-online" className="menu-link">
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/transaksi-obat-online"
+                    )}`}
+                >
+                    <Link
+                        href="/dashboard/transaksi-obat-online"
+                        className="menu-link"
+                    >
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "address-card"]}
+                            icon={["fas", "comments"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Transaksi Obat Online</div>
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/riwayat-kesehatan")}`}>
-                    <Link href="/dashboard/riwayat-kesehatan" className="menu-link">
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/riwayat-kesehatan"
+                    )}`}
+                >
+                    <Link
+                        href="/dashboard/riwayat-kesehatan"
+                        className="menu-link"
+                    >
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
                             icon={["fas", "chart-bar"]}
@@ -95,7 +125,9 @@ const ListItems: React.FC = () => {
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/antar-obat")}`}>
+                <li
+                    className={`menu-item ${isActive("/dashboard/antar-obat")}`}
+                >
                     <Link href="/dashboard/antar-obat" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
@@ -106,22 +138,31 @@ const ListItems: React.FC = () => {
                     </Link>
                 </li>
 
-                <li className={`menu-item ${isActive("/dashboard/jemput-obat")}`}>
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/jemput-obat"
+                    )}`}
+                >
                     <Link href="/dashboard/jemput-obat" className="menu-link">
                         <FontAwesomeIcon
-                            className="menu-icon tf-icons"
+                            className="menu-icon tf-icons "
                             icon={["fas", "truck"]}
                             size="1x"
+                            flip="horizontal"
                         />
                         <div data-i18n="Analytics">Jemput Obat</div>
                     </Link>
                 </li>
-                
-                <li className={`menu-item ${isActive("/dashboard/jadwal-dokter")}`}>
+
+                <li
+                    className={`menu-item ${isActive(
+                        "/dashboard/jadwal-dokter"
+                    )}`}
+                >
                     <Link href="/dashboard/jadwal-dokter" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "truck"]}
+                            icon={["fas", "calendar-days"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Jadwal Dokter</div>
@@ -155,7 +196,7 @@ const ListItems: React.FC = () => {
                     <Link href="/dashboard/apotekers" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "user-doctor"]}
+                            icon={["fas", "user-nurse"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Apoteker</div>
@@ -165,7 +206,7 @@ const ListItems: React.FC = () => {
                     <Link href="/dashboard/operators" className="menu-link">
                         <FontAwesomeIcon
                             className="menu-icon tf-icons"
-                            icon={["fas", "user-doctor"]}
+                            icon={["fas", "user-tie"]}
                             size="1x"
                         />
                         <div data-i18n="Analytics">Operator</div>
