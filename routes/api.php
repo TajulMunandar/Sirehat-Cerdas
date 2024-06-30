ef6ee4638690e48467c61733a3b721da8bb27bc5
TajulMunandar <tajulmunandar701@gmail.com>
2024-06-30 18:29:15 +0700

    dashboard

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', [MobileAuthController::class, 'login']);
 Route::post('/register', [MobileAuthController::class, 'register']);
 Route::get('/profile', [MobileAuthController::class, 'profile']);
